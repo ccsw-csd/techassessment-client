c0767f62df6d752d85565bdef5785af13b97b056
Apq <anibal.picazo@hotmail.com>
2024-03-21 12:05:31 +0100

    enable hashing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 import { routes } from './app.routes';
 import { tokenInterceptor } from './core/services/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withInterceptors([tokenInterceptor])), JwtHelperService, { provide: JWT_OPTIONS, useValue: JWT_OPTIONS }],
+  providers: [provideRouter(routes, withHashLocation()), provideAnimations(), provideHttpClient(withInterceptors([tokenInterceptor])), JwtHelperService, { provide: JWT_OPTIONS, useValue: JWT_OPTIONS }],
 };
